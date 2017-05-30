2c2bcb6cda43d461c313f1f9522b4926dc37634a
David Durieux <d.durieux@siprossii.com>
2017-05-30 12:17:01 +0200

    Add setup + hook file + license file

---
+++ hook.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Notifications plugins
+ *
+ * Copyright (C) 2017-2017 by the Notification plugin Development Team.
+ *
+ * https://github.com/ddurieux/glpi_notifications
+ *
+ * ------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of Notifications plugin project.
+ *
+ * Notifications plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Notifications plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Notifications plugin. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ------------------------------------------------------------------------
+ *
+ * This file is used to manage hooks
+ *
+ * ------------------------------------------------------------------------
+ *
+ * @package   Notifications plugin
+ * @author    David Durieux
+ * @copyright Copyright (c) 2017-2017 Notifications plugin team
+ * @license   AGPL License 3.0 or (at your option) any later version
+ *            http://www.gnu.org/licenses/agpl-3.0-standalone.html
+ * @link      https://github.com/ddurieux/glpi_notifications
+ *
+ */
+
+
+/**
+ * Manage the installation process
+ *
+ * @return boolean
+ */
+function plugin_notifications_install() {
+   return TRUE;
+}
+
+
+
+/**
+ * Manage the uninstallation of the plugin
+ *
+ * @return boolean
+ */
+function plugin_notifications_uninstall() {
+}
---
+++ setup.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * Notifications plugins
+ *
+ * Copyright (C) 2017-2017 by the Notification plugin Development Team.
+ *
+ * https://github.com/ddurieux/glpi_notifications
+ *
+ * ------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of Notifications plugin project.
+ *
+ * Notifications plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Notifications plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Notifications plugin. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ------------------------------------------------------------------------
+ *
+ * This file is used to manage hooks
+ *
+ * ------------------------------------------------------------------------
+ *
+ * @package   Notifications plugin
+ * @author    David Durieux
+ * @copyright Copyright (c) 2017-2017 Notifications plugin team
+ * @license   AGPL License 3.0 or (at your option) any later version
+ *            http://www.gnu.org/licenses/agpl-3.0-standalone.html
+ * @link      https://github.com/ddurieux/glpi_notifications
+ *
+ */
+
+define ("PLUGIN_NOTIFICATIONS_VERSION", "9.1+1.0");
+
+include_once(GLPI_ROOT."/inc/includes.php");
+
+
+/**
+ * Init the hooks of Notifications
+ *
+ * @global array $PLUGIN_HOOKS
+ * @global array $CFG_GLPI
+ */
+function plugin_init_notifications() {
+   global $PLUGIN_HOOKS, $CFG_GLPI;
+
+   $PLUGIN_HOOKS['csrf_compliant']['notifications'] = TRUE;
+
+   $Plugin = new Plugin();
+   $moduleId = 0;
+
+   $debug_mode = false;
+   if (isset($_SESSION['glpi_use_mode'])) {
+      $debug_mode = ($_SESSION['glpi_use_mode'] == Session::DEBUG_MODE);
+   }
+
+   if ($Plugin->isActivated('notifications')) { // check if plugin is active
+
+   }
+}
+
+
+
+/**
+ * Manage the version information of the plugin
+ *
+ * @return array
+ */
+function plugin_version_notifications() {
+   return array('name'           => 'Notifications',
+                'shortname'      => 'notifications',
+                'version'        => PLUGIN_NOTIFICATIONS_VERSION,
+                'license'        => 'AGPLv3+',
+                'author'         => '<a href="mailto:david@durieux.family">David DURIEUX</a>
+                                    & <a href="mailto:dcs.glpi@dcsit-group.com">DCS company</a>',
+                'homepage'       =>'https://github.com/ddurieux/glpi_notifications',
+                'minGlpiVersion' => '9.1'
+   );
+}
+
+
+
+/**
+ * Manage / check the prerequisites of the plugin
+ *
+ * @global object $DB
+ * @return boolean
+ */
+function plugin_notifications_check_prerequisites() {
+
+   return TRUE;
+}
+
+
+
+/**
+ * Check if the config is ok
+ *
+ * @return boolean
+ */
+function plugin_notifications_check_config() {
+   return TRUE;
+}
